5b32cf9198833b48fae0c3c95b0c3420170233af
Ihsaan7 <ihsaan2215@gmail.com>
2025-11-09 14:55:53 +0500

    Add JWT verification for profile route and enhance user route comments

--- src/rotues/user.route.js
+++ src/rotues/user.route.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { loginUser, registerUser } from "../controllers/user.controller.js";
 import { uploadMulter } from "../middlewares/multer.mware.js";
+import verifyJWT from "../middlewares/auth.mware.js";
+import ApiResponse from "../utils/apiResponse.js";
 
 const router = Router();
 
-// User routes
+// ----------------- User routes ------------------------
 router.route("/signup").post(
   uploadMulter.fields([
     {
@@ -18,7 +20,8 @@ router.route("/signup").post(
   ]),
   registerUser
 );
-
 router.route("/login").post(loginUser);
+
+// Protected Routes Here
 
 export default router;
