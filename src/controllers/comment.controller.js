5180baf31f5b1d1b725b8164d376c5a277ff5f8f
Ihsaan7 <ihsaan2215@gmail.com>
2025-11-12 15:17:18 +0500

    Add getAllComment functionality to retrieve comments with pagination and owner details

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -77,4 +77,83 @@ const addComment = asyncHandler(async (req, res) => {
     );
 });
 
-export { addComment };
+const getAllComment = asyncHandler(async (req, res) => {
+  // Get Params
+  const { videoID } = req.params;
+  if (!videoID || !mongoose.isValidObjectId(videoID)) {
+    throw new ApiError(400, "Invalid Video ID!");
+  }
+  const { page = 1, limit = 10 } = req.query;
+
+  const pageNumber = parseInt(page) || 1;
+  const limitNumber = parseInt(limit) || 10;
+
+  // Prevent Over Query
+  if (limitNumber > 100) {
+    throw new ApiError(400, "Limit cannot exceed 100");
+  }
+  if (pageNumber < 1) {
+    throw new ApiError(400, "Page must be at least 1");
+  }
+
+  // Fetch the video
+  const video = await Video.findById(videoID);
+  if (!video) {
+    throw new ApiError(404, "No Video Found!");
+  }
+
+  // Aggregate To get the owner
+  const pipeline = [
+    {
+      $match: { video: new mongoose.Types.ObjectId(videoID) },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+        pipeline: [
+          {
+            $project: {
+              fullName: 1,
+              username: 1,
+              avatar: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: { owner: { $first: "$owner" } },
+    },
+    {
+      $sort: { createdAt: -1 },
+    },
+    {
+      $project: {
+        content: 1,
+        createdAt: 1,
+        updatedAt: 1,
+        owner: 1,
+      },
+    },
+  ];
+
+  // Create the pipline with Aggregate
+  const options = {
+    page: pageNumber,
+    limit: limitNumber,
+  };
+  const result = await Comment.aggregatePaginate(
+    Comment.aggregate(pipeline),
+    options
+  );
+
+  // Return res
+  return res
+    .status(200)
+    .json(new ApiResponse(200, result, "Comments Fetched Successfully"));
+});
+
+export { addComment, getAllComment };
--- src/rotues/comment.route.js
+++ src/rotues/comment.route.js
@@ -1,11 +1,16 @@
 import Router from "express";
-import { addComment } from "../controllers/comment.controller.js";
+import {
+  addComment,
+  getAllComment,
+} from "../controllers/comment.controller.js";
 import verifyJWT from "../middlewares/auth.mware.js";
 
 const router = Router();
 
 // Protected Routes
 router.use(verifyJWT);
-router.route("comment/:videoID").post(addComment);
+router.route("/comment/:videoID").post(addComment);
+
+router.route("/all-comment/:videoID").get(getAllComment);
 
 export default router;
