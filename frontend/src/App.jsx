eb8a030f56ac99544a09248b6d7630a17b1df4d7
Ihsaan7 <ihsaan2215@gmail.com>
2025-11-17 22:05:07 +0500

    feat: implement routing with ProtectedRoute and AuthProvider for authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,31 @@
-import { useState } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
+import LoginPage from "../pages/LoginPage";
+import HomePage from "../pages/HomePage";
+import ProtectedRoute from "../components/ProtectedRoutes";
 
 function App() {
   return (
-    <>
-      <h1 className="text-2xl border-2">Hi am working</h1>
-    </>
+    <Routes>
+      {/* Public Route - Login */}
+      <Route path="/login" element={<LoginPage />} />
+
+      {/* Protected Route - Home */}
+      <Route
+        path="/home"
+        element={
+          <ProtectedRoute>
+            <HomePage />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* Default redirect to login */}
+      <Route path="/" element={<Navigate to="/login" replace />} />
+
+      {/* Catch all - redirect to login */}
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
   );
 }
 
